42fadcab7783f49ec34cfd9588e7c8a507d699a7
AkbharChowdhury <112480476+AkbharChowdhury@users.noreply.github.com>
2022-12-03 02:45:54 +0000

    s

--- app/src/main/java/com/mhike/m_hike/activities/UploadHikeActivity.java
+++ app/src/main/java/com/mhike/m_hike/activities/UploadHikeActivity.java
@@ -5,16 +5,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 import com.mhike.m_hike.R;
 import com.mhike.m_hike.classes.DatabaseHelper;
+import com.mhike.m_hike.classes.JsonThread;
 import com.mhike.m_hike.utilities.Helper;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 public class UploadHikeActivity extends AppCompatActivity {
     private final Activity CURRENT_ACTIVITY = UploadHikeActivity.this;
     private DatabaseHelper db;
     private Context context;
+    private WebView browser;
 
+    // used for by the Json Thread class
+    public WebView getBrowser() {
+        return browser;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +42,23 @@ public class UploadHikeActivity extends AppCompatActivity {
         context = getApplicationContext();
         Helper.getIntentMessage(context, getIntent().getExtras());
         db = DatabaseHelper.getInstance(context);
+
+        browser = (WebView) findViewById(R.id.webkit);
+        try {
+            URL pageURL = new URL(getString(R.string.web_service_url));
+            JsonThread.trustAllHosts();
+            HttpURLConnection con =
+                    (HttpURLConnection) pageURL.openConnection();
+            // this must come from the database
+            String jsonString = getString(R.string.json);
+
+            JsonThread myTask = new JsonThread(this, con, jsonString);
+            Thread t1 = new Thread(myTask, "JSON Thread");
+            t1.start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
     }
 }
---
+++ app/src/main/java/com/mhike/m_hike/classes/JsonThread.java
@@ -0,0 +1,141 @@
+package com.mhike.m_hike.classes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.mhike.m_hike.activities.UploadHikeActivity;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URLEncoder;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+public class JsonThread implements Runnable {
+    private AppCompatActivity activity;
+    private HttpURLConnection con;
+    private String jsonPayLoad;
+
+    public JsonThread(AppCompatActivity activity,
+                      HttpURLConnection con, String jsonPayload) {
+        this.activity = activity;
+        this.con = con;
+        this.jsonPayLoad = jsonPayload;
+    }
+
+
+    @Override
+    public void run() {
+        String response = "";
+        if (prepareConnection()) {
+            response = postJson();
+        } else {
+            response = "Error preparing the connection";
+        }
+        showResult(response);
+    }
+
+
+    private boolean prepareConnection() {
+        try {
+            con.setDoOutput(true);
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Content-Type",
+                    "application/x-www-form-urlencoded");
+            return true;
+
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+
+    private String postJson() {
+        String response = "";
+        try {
+            String postParameters = "jsonpayload="
+                    + URLEncoder.encode(jsonPayLoad, "UTF-8");
+            con.setFixedLengthStreamingMode(postParameters.getBytes().length);
+            PrintWriter out = new PrintWriter(con.getOutputStream());
+            out.print(postParameters);
+            out.close();
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                response = readStream(con.getInputStream());
+            } else {
+                response = "Error contacting server: " + responseCode;
+            }
+        } catch (Exception e) {
+            response = "Error executing code";
+        }
+        return response;
+    }
+
+
+    private void showResult(String response) {
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                String page = generatePage(response);
+                ((UploadHikeActivity) activity).getBrowser().loadData(page,
+                        "text/html", "UTF-8");
+            }
+        });
+    }
+
+    private String generatePage(String content) {
+        return "<html><body><p>" + content + "</p></body></html>";
+    }
+
+    private String readStream(InputStream in) {
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(in))) {
+            String nextLine = "";
+            while ((nextLine = reader.readLine()) != null) {
+                sb.append(nextLine);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
+    }
+
+    public static void trustAllHosts() {
+        // Create a trust manager that does not validate certificate chains
+        TrustManager[] trustAllCerts = new TrustManager[]{
+                new X509TrustManager() {
+                    public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                        return new java.security.cert.X509Certificate[]{};
+                    }
+
+                    public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+                    }
+
+                    public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+                    }
+                }
+        };
+        // Install the all-trusting trust manager
+        try {
+            SSLContext sc = SSLContext.getInstance("TLS");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection
+                    .setDefaultSSLSocketFactory(sc.getSocketFactory());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
